5cb86a738b366688b8c8a5bdd2537e59c42f0f5c
nan2iz <nan2iz.ch@gmail.com>
2017-03-09 16:58:15 -0800

    update test for chrome browser

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
 
+#firefox
 driver = webdriver.Firefox(executable_path=r'c:\lib\geckodriver.exe')
+driver.get("http://bronco-planner.com/")
+assert "Bronco-Planner - CPP Student Organizer" in driver.title
+driver.close()
+
+#chrome
+driver = webdriver.Chrome(executable_path=r'c:\lib\chromedriver.exe')
 driver.get("http://bronco-planner.com/")
 assert "Bronco-Planner - CPP Student Organizer" in driver.title
 driver.close()
