ecba2c7531cf7ce6ce5c25362486c8273aeb0fc8
nan2iz <nan2iz.ch@gmail.com>
2017-03-09 17:18:51 -0800

    add basic unit test

---
+++ TestSample.py
@@ -0,0 +1,19 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class TestSample(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox(executable_path=r'c:\lib\geckodriver.exe')
+
+    def test_search_in_website(self):
+        driver = self.driver
+        driver.get("https://www.cpp.edu/")
+        self.assertIn("Cal Poly Pomona", driver.title)
+        elem = driver.find_element_by_name("viewport")
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
