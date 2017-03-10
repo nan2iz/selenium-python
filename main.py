45f95eac90d76c70d05482f5b1fcfacacabf9e56
nan2iz <nan2iz.ch@gmail.com>
2017-03-09 16:56:05 -0800

    update basic selenium test

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from selenium import webdriver
 
+driver = webdriver.Firefox(executable_path=r'c:\lib\geckodriver.exe')
+driver.get("http://bronco-planner.com/")
+assert "Bronco-Planner - CPP Student Organizer" in driver.title
+driver.close()
